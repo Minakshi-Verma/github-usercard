049b9f10bc3ab697e245073930c70d56bbbd0b5f
Minakshi Verma <mvedwan@gmail.com>
2020-01-16 21:56:52 -0500

    "broken code fixed!"

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,11 +3,13 @@
 //step-1 creating .get request and check if it is working
 
 const myArr = [ 
-  "tetondan",
-  "dustinmyers",
-  "justsml",
-  "luishrd",
-  "bigknell"
+  "jasheloper",
+  "Keyeric",
+  "lgv-0",
+  "tatek1993",
+  "Dlray89",
+  
+  "alexvision26"
 
 ]
 
@@ -16,7 +18,7 @@ entryPoint = document.querySelector(".cards")
 
 // axios.get("https://api.github.com/users/Minakshi-Verma")
 // .then(response=>{
-//   console.log(response,"I am a flag for the axios response")
+//   console.log(response)
 //   response.data.forEach(item => {
 //     const newdata = card_creater(item);
     
@@ -30,7 +32,7 @@ entryPoint = document.querySelector(".cards")
 myArr.forEach(person =>{
   axios.get(`https://api.github.com/users/${person}`)
   .then(response => {
- 
+  
  entryPoint.append(cardCreater(response))
   })
   .catch(error => {
@@ -40,24 +42,21 @@ myArr.forEach(person =>{
 })
 
 
-
-
-
 function cardCreater(obj){
 
   //---create all the elements---
   
-        card = document.createElement("div");
-        image = document.createElement("img");
-        cardInfo = document.createElement("div");
-        name = document.createElement("h3");
-        username = document.createElement("p");
-        location = document.createElement("p");
-        profile = document.createElement("p");
-        profileLink = document.createElement("a");
-        followers = document.createElement("p");
-        following = document.createElement("p");
-        bio = document.createElement("p");
+      const card = document.createElement("div");
+      const image = document.createElement("img");
+      const cardInfo = document.createElement("div");
+      const name = document.createElement("h3");
+      const username = document.createElement("p");
+      const location = document.createElement("p");
+      const profile = document.createElement("p");
+      const profileLink = document.createElement("a");
+      const followers = document.createElement("p");
+      const following = document.createElement("p");
+      const bio = document.createElement("p");
 
        //---append to parent
 
@@ -85,17 +84,16 @@ function cardCreater(obj){
         name.textContent = obj.data.name;
         username.textContent = obj.data.login;
         location.textContent = obj.data.location;
-        // profile.textContent = obj.data.url
-            profile.textContent = "Profile: ";
-            profileLink.href = obj.data.html_url;
-            var text = document.createTextNode(obj.data.url);
-            profileLink.append(text);
-            profile.append(profileLink);
+        profile.textContent = obj.data.url
+            // profile.textContent = "Profile: ";
+            // profileLink.href = obj.data.html_url;
+            // var text = document.createTextNode(obj.data.url);
+            // profileLink.append(text);
+            // profile.append(profileLink);
 
-
-        followers.textContent = "Followers: " + obj.data.followers;
-        following.textContent = "Following: " + obj.data.following;
-        bio.textContent = "Bio: " + obj.data.bio; 
+            followers.textContent = "Followers: " + obj.data.followers;
+            following.textContent = "Following: " + obj.data.following;
+            bio.textContent = "Bio: " + obj.data.bio; 
 
   return card
 }
